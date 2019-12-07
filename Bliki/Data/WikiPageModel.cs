152ec308d9f5a358e39cacdd871bef61d12a8bb2
Tim Purdum <tpurdum@idnetworks.com>
2019-12-07 15:26:58 -0600

    Saving file page-two at 12/7/2019 3:26:57 PM

--- Bliki/Data/WikiPageModel.cs
+++ Bliki/Data/WikiPageModel.cs
@@ -3,7 +3,7 @@
     public class WikiPageModel
     {
         public string? Title { get; set; }
-        public string? PageLink { get; set; }
+        public string PageLink { get; set; } = "new-page";
         public string Content { get; set; } = "";
         public string SubTitle { get; set; } = "";
     }
